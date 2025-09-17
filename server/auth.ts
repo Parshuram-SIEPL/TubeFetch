3aae98bc2f2b357b336ba9b31d38412dc5cc0c25
Parshuram Behera <parshuram.behera.surya@gmail.com>
2025-09-17 16:08:42 +0530

    Update auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -1,8 +1,20 @@
 // server/auth.ts
 import { Request, Response, NextFunction } from 'express';
 
+// Accept both Authorization: Bearer ... and X-Admin-Token
 export function requireAdmin(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization?.split(' ')[1];
+  let token = '';
+  // Support Authorization: Bearer ...
+  if (req.headers.authorization) {
+    const split = req.headers.authorization.split(' ');
+    if (split.length === 2 && split[0].toLowerCase() === 'bearer') {
+      token = split[1];
+    }
+  }
+  // Support X-Admin-Token header
+  if (!token && typeof req.headers['x-admin-token'] === 'string') {
+    token = req.headers['x-admin-token'] as string;
+  }
   if (token && token === process.env.ADMIN_TOKEN) {
     next();
   } else {
